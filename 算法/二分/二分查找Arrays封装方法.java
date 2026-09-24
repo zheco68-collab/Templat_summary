729b9ff37413c735cd191b284a23a56c59e2d774
zheco68 <zheco68@gmail.com>
2026-09-24 15:19:15 +0800

    添加二分查找算法模板

---
+++ 算法/二分/二分查找Arrays封装方法.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class 二分查找模板 {
+    public static void main(String... args) throws Exception {
+        int[] a = {1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.binarySearch(a,5));
+    }
+}
